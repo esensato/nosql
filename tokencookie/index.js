bf87eea6f85a4299320d16bda486cea1ab94a4a3
Edson A. Sensato <esensato@hotmail.com>
2020-03-11 21:34:25 -0300

    Aula 11/04

--- tokencookie/index.js
+++ tokencookie/index.js
@@ -2,15 +2,39 @@
 // importacao dos modulos necessarios
 var express = require('express')
 var redis = require('redis')
+var cookie = require('cookie-parser')
 
 // instanciar o servidor de aplicacao
 var app = express()
 
+// ativa uso de cookies na aplicacao
+app.use(cookie())
+// define que o conteudo estatico sera colocado na pasta public
+app.use(express.static('public'))
+// ativar a manipulacao de formularios html (form)
+app.use(express.urlencoded({ extended: true }))
+
 // responder a requisicoes do tipo GET
 app.get('/', function (req, res){
+
+	// verifica se ja possui o cookie com o token
+	var temCookie = req.cookies.tokencookie
 	
-	res.send('<h1>Servidor no ar!</h1>')
+	if (temCookie) {
+		res.send('<h1>Cookie - SIM!</h1>')		
+	} else {
+		// retorna a pagina de login localizada em .../tokencookie/public/login.html
+		res.sendFile(__dirname + "/public/login.html");
+	}
 	
+})
+
+// responder ao login (POST)
+app.post('/login', function (req, res){
+	var username = req.body.username // acesso ao conteudo do campo username do form login.html
+	var senha = req.body.senha
+	console.log('Username = ' + username)
+	res.send('ok')
 })
 
 app.get('/testeredis', function (req, res){
